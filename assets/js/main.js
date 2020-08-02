9a0373a34d1d91ddca302f560ded4dac0a5c44f2
piensaenpixel <piensaenpixel@gmail.com>
2020-08-02 20:42:57 +0200

    add footer and icon animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,6 +67,22 @@
     } else {
       document.querySelector('.bg').className += ' open'
     }
+    if (document.querySelector('.header-action ').classList.contains('open')) {
+      document.querySelector('.header-action').classList.remove('open')
+    } else {
+      document.querySelector('.header-action').className += ' open'
+    }
   });
+
+  var quotes = [
+    "Con su boina calada, con sus guantes de seda. Su sirena varada, sus fiestas de guardar.",
+    "Su vuelva usted mañana, su sálvese quien pueda. Su partidita de mus, su fulanita de tal.",
+    "Con su todo es ahora, con su nada es eterno. Con su rap y su chotis, con su okupa y su skin.",
+    "Aunque muera el verano y tenga prisa el invierno, la primavera sabe que la espero en Madrid"
+  ];
+
+  var q = quotes[ Math.floor( Math.random() * quotes.length ) ];
+  document.getElementById("footer-text").innerHTML = q;  
+
 
 })();
